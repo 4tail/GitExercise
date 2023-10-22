de78c0c16d901f0f734bad2346ec47a9f40f02ae
4tail <148640255+4tail@users.noreply.github.com>
2023-10-22 13:34:54 +0300

    Add files via upload

--- GitExercise.py
+++ GitExercise.py
@@ -1,28 +1,32 @@
 class Work:
-    def __init__(self , volt) :
+    def __init__(self , volt,maxvolt,numofbits) :
         self.volt = volt
-    
+        self.maxvolt = maxvolt
+        self.numofbits = numofbits
+        
+    #((2**self.numberofbits) - 1)
     #1023
     def ToDigital(self):
-        num = self.volt/5
-        numberofdigit = num*1023
+        num = self.volt/self.maxvolt
+        numberofdigit = num*((2**self.numofbits) - 1)
         return(bin(int(numberofdigit))[2:])
     
     def SetDigiotalValue(self,value):
         
         num = int(str(value),2)
-        self.volt = (num/1023)*5
+        self.volt = (num/((2**self.numofbits) - 1))*self.maxvolt
 
 
 
     
     def printvolt(self):
         print(self.volt)
+    
 
 
 
-t1 = Work(2)
 
+t1 = Work(15,20,8)
 print(t1.ToDigital())
-num = 1100110010
-t1.SetDigiotalValue(int(num))     
+t1.SetDigiotalValue(10011001)
+t1.printvolt()
